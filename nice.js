37c0fb4efc27ea40946087a907a9542ed6ecf443
Luck7702 <dennisch2206@gmail.com>
2025-03-20 21:15:53 +0700

    yep this is fine

--- nice.js
+++ nice.js
@@ -1,103 +1,53 @@
 
-let x = ""
+let x = ""; let y = 1; let total = 0;
 
-let y = 1;
+let z = 0;
 
-let total = 0;
+let colors = ["aquamarine","red","blue","green"];
+
+document.querySelectorAll("#Numbers button").forEach(button => {
+    button.onclick = function() {
+        x += this.textContent; 
+        document.getElementById("output").textContent = x;
+    };
+});
+
+document.getElementById("AC").onclick = function(){
+
+    total = 0; x = ""; y = 1;
+    document.getElementById("output").textContent = 0;
 
 
-
-
-document.getElementById("1").onclick = function(){
-    x += "1";
-    document.getElementById("output").textContent = x;
-}
-
-
-document.getElementById("2").onclick = function(){
-    x += "2";
-    document.getElementById("output").textContent = x;
-}
-
-document.getElementById("3").onclick = function(){
-    x += "3";
-    document.getElementById("output").textContent = x;
-}
-
-document.getElementById("4").onclick = function(){
-    x += "4";
-    document.getElementById("output").textContent = x;
-}
-
-document.getElementById("5").onclick = function(){
-    x += "5";
-    document.getElementById("output").textContent = x;
-}
-
-document.getElementById("6").onclick = function(){
-    x += "6";
-    document.getElementById("output").textContent = x;
-}
-
-document.getElementById("7").onclick = function(){
-    x += "7";
-    document.getElementById("output").textContent = x;
-}
-
-document.getElementById("8").onclick = function(){
-    x += "8";
-    document.getElementById("output").textContent = x;
-}
-
-document.getElementById("9").onclick = function(){
-    x += "9";
-    document.getElementById("output").textContent = x;
-}
-
-document.getElementById("0").onclick = function(){
-    x += "0";
-    document.getElementById("output").textContent = x;
 }
 
 document.getElementById("+").onclick = function(){
 
-   
-
     total += y * (document.getElementById("output").textContent)
-
-    y = 1
-    x = ""
-
+    y = 1; x = "";  
     document.getElementById("output").textContent = 0;
-
-    
-
 }
 
 document.getElementById("-").onclick = function(){
 
-    
-    
     total += y * (document.getElementById("output").textContent)
-    y = -1;
-    x = "";
+    y = -1; x = "";
     document.getElementById("output").textContent = 0;
-
 }
 
 document.getElementById("=").onclick = function(){
     
-    
     total += y * (document.getElementById("output").textContent);
-
-    x = "";
-    y = 1;
-    
+    x = ""; y = 1;
     document.getElementById("output").textContent = total;
-
     total = 0;
-
-    
-
     console.log(y);
+}
+
+document.getElementById("Color").onclick = function(){
+
+    if(z >= 3) z = -1; z += 1;
+    
+    document.getElementById("calcbutt").style.backgroundColor = colors[z];
+    console.log(z);
+
 }
